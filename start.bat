a9af634803fe2c06f69449cac948980a975384bd
micha <michal.koscinskiilo@gmail.com>
2023-06-11 23:47:17 +0200

    fixes

--- start.bat
+++ start.bat
@@ -2,6 +2,7 @@
 
 if %errorlevel% neq 0 msg %username% "Nie udalo sie utworzyc bazy danych." && exit /b %errorlevel%
 
+call composer update
 call composer install
 call php artisan migrate:fresh --seed
 call php artisan key:generate
